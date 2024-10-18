4d52f6096f2e6632452a1f1ac54d262f357471f6
hulinhui6 <975081281@qq.com>
2024-10-18 18:11:36 +0800

    20241018kp脚本完善2.14

--- QuestionCard/KpRequest/KpUploadFile.py
+++ QuestionCard/KpRequest/KpUploadFile.py
@@ -1,18 +1,42 @@
 from QuestionCard.KpRequest.KpLogin import KpLogin
 from QuestionCard.PdfConvertImage import get_file_path
+from openpyxl import Workbook
 
 
 class KpUploadFile:
-    def __init__(self):
+    def __init__(self, up_type):
         self.object = KpLogin()
+        self.workbook = Workbook()
+        self.sheet = self.workbook.active
+        self.up_list = ['stu', 'tea', 'score']
+        self.upload_type = self._check_type(up_type)
+
+    def _check_type(self, up_type):
+        if not isinstance(up_type, str):
+            raise TypeError(f'Type of {up_type} is not str')
+        if up_type not in self.up_list:
+            raise Exception("上传类型不满足条件")
+        return up_type
+
+    def __del__(self):
+        self.workbook.close()
+
+    def append_cols(self, cols_list):
+        last_column_index = self.sheet.max_column
+        for index, col in enumerate(cols_list, 1):
+            cols_index = last_column_index + index
+            self.sheet.insert_cols(cols_index)
+            self.sheet.cell(row=1, column=cols_index).value = col
+
+    def delete_cols(self):
+        pass
 
     def create_excel_data(self):
         pass
 
-    def get_upload_info(self, upload_type):
+    def get_upload_info(self):
         """
         获取上传所需数据
-        :param upload_type: stu-学生上传、tea-老师上传、score-分数上传
         :return: tuple  上传url、上传data、上传文件路径
         """
         data = self.object.kp_data
@@ -23,16 +47,15 @@ class KpUploadFile:
             'tea': (data['upload_tea_url'], upload_data, get_file_path('阅卷老师导入模板.xlsx', folder_path)),
             'score': (data['upload_score_url'], upload_data, get_file_path('上传成绩导入模版.xlsx', folder_path))
         }
-        return upload_item.get(upload_type, "无效的输入")
+        return upload_item.get(self.upload_type, "无效的输入")
 
-    def upload_file(self, up_type):
+    def upload_file(self):
         """
         通过文件上传发送请求
-        :param up_type: 上传类型
         :return: None
         """
         # 请求URL、请求data(同时使用files和data参数，通常会将data也作为一个字典来传递)
-        upload_url, upload_data, file_path = self.get_upload_info(up_type)
+        upload_url, upload_data, file_path = self.get_upload_info()
         # 清除Content-Type字段(files参数会自动帮我们加上Content-Type,如果我传了就会覆盖自动添加的，导致请求失败)
         del self.object.headers['Content-Type']
         # 请求文件对象
@@ -56,9 +79,9 @@ class KpUploadFile:
         # 登录
         self.object.get_login_token()
         # 上传文件
-        self.upload_file(up_type='stu')
+        self.upload_file()
 
 
 if __name__ == '__main__':
-    uf = KpUploadFile()
+    uf = KpUploadFile(up_type='stu')
     uf.run()
