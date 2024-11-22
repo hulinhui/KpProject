d7c99938563bab5a881d6422b22aba001e7516a1
hulinhui6 <975081281@qq.com>
2024-11-22 18:41:00 +0800

    20241122kp脚本完善2.21

--- QuestionCard/KpRequest/KpMarking.py
+++ QuestionCard/KpRequest/KpMarking.py
@@ -4,12 +4,12 @@ class KpMarking:
         self.data = self.login_object.kp_data
 
     def query_task_exam(self, orgid, orgtype, userid):
-        examtask_url = self.data['examtask_url']
-        examTask_data = {"data": {"userId": userid, "orgType": orgtype, "orgId": orgid,
-                                  "roleCodes": ["ROLE_ORG_MANAGER", "ROLE_CLASS_TEACHER", "ROLE_TEACHER"],
-                                  "examName": self.data['exam_name']}, "pageSize": 5, "pageNum": 1}
-        examTask_response = self.login_object.get_response(url=examtask_url, method='POST', data=examTask_data)
-        result, r_data = self.login_object.check_response(examTask_response)
+        eTask_url = self.data['examtask_url']
+        eTask_data = {"data": {"userId": userid, "orgType": orgtype, "orgId": orgid,
+                               "roleCodes": ["ROLE_ORG_MANAGER", "ROLE_CLASS_TEACHER", "ROLE_TEACHER"],
+                               "examName": self.data['exam_name']}, "pageSize": 5, "pageNum": 1}
+        eTask_response = self.login_object.get_response(url=eTask_url, method='POST', data=eTask_data)
+        result, r_data = self.login_object.check_response(eTask_response)
         if result:
             data = r_data.get("data")
             exam_id = data and data[0]['examId'] or None
@@ -17,8 +17,16 @@ class KpMarking:
         else:
             self.login_object.logger.info('获取响应失败!')
 
-    def query_task_paper(self):
-        pass
+    def query_task_paper(self, exam_id):
+        pTask_url = self.data['papertask_url']
+        pTask_data = {"examId": exam_id}
+        pTask_response = self.login_object.get_response(url=pTask_url, method='POST', data=pTask_data)
+        result, r_data = self.login_object.check_response(pTask_response)
+        if result:
+            data = r_data.get("data")
+            return data
+        else:
+            self.login_object.logger.info('获取响应失败!')
 
     def get_normal_task(self):
         pass
@@ -26,7 +34,8 @@ class KpMarking:
     def run(self):
         login_info = self.login_object.get_login_token(keys=['orgType', 'userId'])
         exam_id = self.query_task_exam(*login_info)
-        print(exam_id)
+        data = self.query_task_paper(exam_id)
+        print(data)
 
 
 if __name__ == '__main__':
