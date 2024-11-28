d6a63ab06d9c504c058bf412d9d6cb3ceca0ab96
hulinhui6 <975081281@qq.com>
2024-11-28 17:17:59 +0800

    20241126kp脚本完善2.24

--- QuestionCard/KpRequest/KpMarking.py
+++ QuestionCard/KpRequest/KpMarking.py
@@ -7,7 +7,14 @@ class KpMarking:
         self.login_object = l_object
         self.data = self.login_object.kp_data
 
-    def query_task_exam(self, orgid, orgtype, userid):
+    def query_task_examid(self, orgid, orgtype, userid):
+        """
+        查询在线阅卷页面的考试id
+        :param orgid: 机构id
+        :param orgtype:机构类型
+        :param userid:登录用户id
+        :return: exam_id
+        """
         eTask_url = self.data['examtask_url']
         eTask_data = {"data": {"userId": userid, "orgType": orgtype, "orgId": orgid,
                                "roleCodes": ["ROLE_ORG_MANAGER", "ROLE_CLASS_TEACHER", "ROLE_TEACHER"],
@@ -22,6 +29,11 @@ class KpMarking:
             self.login_object.logger.info('获取响应失败!')
 
     def query_task_paper(self, exam_id):
+        """
+        查询考试下指定科目的阅卷任务数据
+        :param exam_id: 考试id
+        :return: list 科目阅卷数据
+        """
         pTask_url = self.data['papertask_url']
         pTask_data = {"examId": exam_id}
         pTask_response = self.login_object.get_response(url=pTask_url, method='POST', data=pTask_data)
@@ -35,6 +47,11 @@ class KpMarking:
             self.login_object.logger.info('获取响应失败!')
 
     def get_normal_task(self, exam_info):
+        """
+        获取正常卷：阅卷完成情况及可需要阅卷的题组
+        :param exam_info: 考试数据
+        :return:
+        """
         if exam_info:
             paper_name = exam_info.pop('paper_name')
             divtask_list = exam_info.pop('paper_info').get('taskList', [])
@@ -48,6 +65,11 @@ class KpMarking:
             self.login_object.logger.info('当前科目暂无阅卷任务')
 
     def get_third_task(self, exam_info):
+        """
+        获取三评卷：阅卷完成情况及可需要阅卷的题组
+        :param exam_info: 考试数据
+        :return:
+        """
         if exam_info:
             paper_name = exam_info.pop('paper_name')
             paper_info = exam_info.pop('paper_info')
@@ -60,6 +82,11 @@ class KpMarking:
             self.login_object.logger.info('当前科目暂无阅卷任务')
 
     def get_problem_task(self, exam_info):
+        """
+        获取问题卷：阅卷完成情况及可需要阅卷的题组
+        :param exam_info: 考试数据
+        :return:
+        """
         if exam_info:
             paper_name = exam_info.pop('paper_name')
             paper_info = exam_info.pop('paper_info')
@@ -72,6 +99,11 @@ class KpMarking:
             self.login_object.logger.info('当前科目暂无阅卷任务')
 
     def get_arbitration_task(self, exam_info):
+        """
+        获取仲裁卷：阅卷完成情况及可需要阅卷的题组
+        :param exam_info: 考试数据
+        :return:
+        """
         if exam_info:
             paper_name = exam_info.pop('paper_name')
             paper_info = exam_info.pop('paper_info')
@@ -84,6 +116,11 @@ class KpMarking:
             self.login_object.logger.info('当前科目暂无阅卷任务')
 
     def query_div_detail(self, div_dict):
+        """
+        获取题组的给分点信息
+        :param div_dict: 考试信息
+        :return:
+        """
         div_item = {}
         div_url = self.data['divdetail_url']
         div_data = {"data": div_dict}
@@ -100,6 +137,13 @@ class KpMarking:
         return div_item
 
     def div_reques_task(self, req_url, div_dict, data_type=1):
+        """
+        获取阅卷任务通用方法，包含（正常卷、问题卷、三评卷、仲裁卷）
+        :param req_url: 获取阅卷任务请求url
+        :param div_dict: 考试信息
+        :param data_type: 传参类型（1：正常卷和三评卷；2：问题卷与仲裁卷）
+        :return:考生密号、卷类型、任务id 组成的元祖
+        """
         req_data = {"data": div_dict} if data_type == 1 else div_dict
         req_response = self.login_object.get_response(url=req_url, method='POST', data=req_data)
         result, r_data = self.login_object.check_response(req_response)
@@ -115,7 +159,14 @@ class KpMarking:
             self.login_object.logger.info(error_msg)
             return (result,)
 
-    def generate_random_score(self, point_item, task_item, score_type=1):
+    def generate_random_score(self, point_item, task_tuple, score_type=1):
+        """
+        生成每个给分点的分数，并组成完整提交给分的参数
+        :param point_item: 包含给分点的考生数据字典
+        :param task_tuple: 阅卷任务接口返回的元祖信息（考生密号、卷类型、任务id）
+        :param score_type: 给分方式（0：零分；1：范围内随机分数；2：满分）
+        :return: 完整的提交给分参数
+        """
         if not point_item: return
         for score_item in point_item.get('scorePoints', []):
             end, step = score_item.get('total_score'), score_item.get('step')
@@ -125,9 +176,15 @@ class KpMarking:
             self.login_object.logger.info(f'本次评分为：{score}')
             score_item.update({'score': score})
         # 解包更新提交数据所需字段
-        point_item.update(zip(['encode', 'pjSeq', 'taskId'], task_item))
+        point_item.update(zip(['encode', 'pjSeq', 'taskId'], task_tuple))
 
     def req_submit_score(self, url, submit_data):
+        """
+        提交给分
+        :param url: 提交给分的url
+        :param submit_data: 提交给分的请求参数
+        :return:
+        """
         if not submit_data: return
         normal_data = {"data": submit_data}
         normal_response = self.login_object.get_response(url=url, method='POST', data=normal_data)
@@ -138,7 +195,29 @@ class KpMarking:
             error_msg = r_data.get("errorCode") and r_data.get("errorMsg") or '获取响应失败!'
             self.login_object.logger.info(error_msg)
 
+    def exam_paper_info(self):
+        """
+        获取由exam_Id,paperId,paper_info组成的字典数据，给后续提交给分接口提供参数
+        :return: dict
+        """
+        login_info = self.login_object.get_login_token(keys=['orgType', 'userId'])
+        exam_id = self.query_task_examid(*login_info)
+        if not exam_id:
+            self.login_object.logger.info('获取exam_id参数有误')
+            return None
+        paper_info = self.query_task_paper(exam_id)
+        if not paper_info:
+            self.login_object.logger.info('获取paper_id参数有误')
+            return None
+        paper_id, paper_name, paper_info = paper_info[0]
+        return {'examId': exam_id, 'paperId': paper_id, 'paper_name': paper_name, 'paper_info': paper_info}
+
     def submit_normal_score(self, exam_data):
+        """
+        正常卷给分
+        :param exam_data: 考试数据
+        :return:
+        """
         div_data = self.get_normal_task(exam_data)
         if not div_data: return
         div_alias = div_data.pop('div_alias')
@@ -157,6 +236,11 @@ class KpMarking:
         self.login_object.logger.info('阅卷结束！')
 
     def submit_thrid_score(self, exam_data):
+        """
+        三评卷给分
+        :param exam_data: 考试数据
+        :return:
+        """
         third_data = self.get_third_task(exam_data)
         if not third_data: return
         div_alias = third_data.pop('div_alias')
@@ -175,6 +259,11 @@ class KpMarking:
         self.login_object.logger.info('阅卷结束！')
 
     def submit_problem_score(self, exam_data):
+        """
+        问题卷给分
+        :param exam_data: 考试数据
+        :return:
+        """
         problem_data = self.get_problem_task(exam_data)
         if not problem_data: return
         div_alias = problem_data.pop('div_alias')
@@ -193,6 +282,11 @@ class KpMarking:
         self.login_object.logger.info('阅卷结束！')
 
     def submit_arbitration_score(self, exam_data):
+        """
+        仲裁卷给分
+        :param exam_data: 考试数据
+        :return:
+        """
         arbit_data = self.get_arbitration_task(exam_data)
         if not arbit_data: return
         div_alias = arbit_data.pop('div_alias')
@@ -210,25 +304,16 @@ class KpMarking:
                 if not any(task_result): break
         self.login_object.logger.info('阅卷结束！')
 
-    def exam_paper_info(self):
-        login_info = self.login_object.get_login_token(keys=['orgType', 'userId'])
-        exam_id = self.query_task_exam(*login_info)
-        if not exam_id:
-            self.login_object.logger.info('获取exam_id参数有误')
-            return None
-        paper_info = self.query_task_paper(exam_id)
-        if not paper_info:
-            self.login_object.logger.info('获取paper_id参数有误')
-            return None
-        paper_id, paper_name, paper_info = paper_info[0]
-        return {'examId': exam_id, 'paperId': paper_id, 'paper_name': paper_name, 'paper_info': paper_info}
-
     def run(self):
+        """
+        主流程
+        :return:
+        """
         exam_data = self.exam_paper_info()
         self.submit_normal_score(exam_data)
-        # self.submit_thrid_score(exam_data)
-        # self.submit_problem_score(exam_data)
-        # self.submit_arbitration_score(exam_data)
+        self.submit_thrid_score(exam_data)
+        self.submit_problem_score(exam_data)
+        self.submit_arbitration_score(exam_data)
 
 
 if __name__ == '__main__':
